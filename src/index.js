db17f22100848dbbd1719f399a857626acb9934a
Fredrik Bondza <fredrik.bondza@strossle.com>
2016-06-30 14:17:39 +0200

    Export everything through index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+const Metrics = require('./metrics.js');
+const Stdout = require('./output/stdout-output.js');
+const Tcp = require('./output/tcp-output.js');
+const BufferedTcp = require('./output/buffered-tcp-output.js');
+const Udp = require('./output/udp-output.js');
+const BufferedUdp = require('./output/buffered-udp-output.js');
+const InfluxdbLineProtocol = require('./formatter/influxdb-line-protocol-formatter.js');
 
+Metrics.Output = {
+    Stdout,
+    Tcp,
+    BufferedTcp,
+    Udp,
+    BufferedUdp,
+};
+Metrics.Formatter = {
+    InfluxdbLineProtocol,
+};
+
+module.exports = Metrics;
