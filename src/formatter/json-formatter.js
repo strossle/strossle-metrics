a88b2f0962e9c4bd9010124443151b4b7cf9a97e
gictor <victor.lindellee@strossle.com>
2016-07-06 11:12:55 +0200

    Add json formatter

---
+++ src/formatter/json-formatter.js
@@ -0,0 +1,10 @@
+const Formatter = require('./formatter.js');
+
+class JSONFormatter extends Formatter {
+    format(measurement, values, optionals = {}) {
+        const { timestamp } = optionals;
+        return JSON.stringify({ measurement, values, timestamp });
+    }
+}
+
+module.exports = JSONFormatter;
