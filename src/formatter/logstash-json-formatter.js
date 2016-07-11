42eac7a125c73f9b47095ded4eb9c0acacc4dc80
Fredrik Bondza <fredrik.bondza@strossle.com>
2016-07-11 10:36:07 +0200

    Do not add an empty timestamp property

--- src/formatter/logstash-json-formatter.js
+++ src/formatter/logstash-json-formatter.js
@@ -3,7 +3,12 @@ const Formatter = require('./formatter.js');
 class LogstashJSON extends Formatter {
     format(measurement, values, optionals = {}) {
         const { tags, timestamp, pluginData } = optionals;
-        const data = Object.assign({ measurement, '@timestamp': timestamp }, values, pluginData);
+        let data;
+        if (timestamp) {
+            data = Object.assign({ measurement, '@timestamp': timestamp }, values, pluginData);
+        } else {
+            data = Object.assign({ measurement }, values, pluginData);
+        }
         return `${JSON.stringify(data)}\n`;
     }
 }
