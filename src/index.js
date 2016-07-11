c37025f56d0b78b70d8759c9aad4eabde0921593
Fredrik Bondza <fredrik.bondza@strossle.com>
2016-07-11 10:13:05 +0200

    Export plugins

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+require('isomorphic-fetch');
+
 const Metrics = require('./metrics.js');
+
+/* Formatters */
+const InfluxdbLineProtocol = require('./formatter/influxdb-line-protocol-formatter.js');
+const JSONFormatter = require('./formatter/json-formatter.js');
+const LogstashJSON = require('./formatter/logstash-json-formatter.js');
+
+/* Plugins */
+const Rancher = require('./plugin/rancher-plugin.js');
+const Npm = require('./plugin/npm-plugin.js');
+const Node = require('./plugin/node-plugin.js');
+
+/* Outputs */
 const Stdout = require('./output/stdout-output.js');
 const Tcp = require('./output/tcp-output.js');
 const BufferedTcp = require('./output/buffered-tcp-output.js');
 const Udp = require('./output/udp-output.js');
 const BufferedUdp = require('./output/buffered-udp-output.js');
-const InfluxdbLineProtocol = require('./formatter/influxdb-line-protocol-formatter.js');
-const JSONFormatter = require('./formatter/json-formatter.js');
 
 Metrics.Output = {
     Stdout,
@@ -14,9 +26,17 @@ Metrics.Output = {
     Udp,
     BufferedUdp,
 };
+
+Metrics.Plugin = {
+    Rancher,
+    Npm,
+    Node,
+};
+
 Metrics.Formatter = {
     InfluxdbLineProtocol,
-    JSON:JSONFormatter,
+    LogstashJSON,
+    JSON: JSONFormatter,
 };
 
 module.exports = Metrics;
